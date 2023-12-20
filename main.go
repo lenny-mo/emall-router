09e17438b628873f8e453dd89e56c51673cd7a53
Lenny Mo <lennymo.work@gmail.com>
2023-12-20 20:14:45 +0800

    add tracing middleware on router

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/lenny-mo/emall-utils/tracer"
 	"github.com/lenny-mo/router/global"
 	"github.com/lenny-mo/router/router"
+	"github.com/opentracing/opentracing-go"
 
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/web"
 	"github.com/micro/go-plugins/registry/consul/v2"
+	opentracing2 "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
 )
 
 func main() {
@@ -21,12 +24,24 @@ func main() {
 		}
 	})
 
+	rpcServiceName := "go.micro.service.router.globalRPC"
+
+	// 链路追踪
+	err := tracer.InitTracer(rpcServiceName, "127.0.0.1:6831")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tracer.Closer.Close()
+	opentracing.SetGlobalTracer(tracer.Tracer)
+
 	// 创建rpc服务
 	global.GlobalRPCService = micro.NewService(
-		micro.Name("go.micro.service.router.globalRPC"),
+		micro.Name(rpcServiceName),
 		micro.Version("latest"),
 		micro.Address("127.0.0.1:8098"),
 		micro.Registry(consulRegistry),
+		micro.WrapClient(opentracing2.NewClientWrapper(opentracing.GlobalTracer())),
 	)
 	global.GlobalRPCService.Init()
 
@@ -44,9 +59,12 @@ func main() {
 		web.Name("go.micro.service.router"),
 		web.Address("127.0.0.1:8099"),
 		web.Handler(ginRouter),
-		web.Registry(consulRegistry))
+		web.Registry(consulRegistry),
+	)
 
 	webService.Init() // 启动web server 服务器
+
+	tracer.InitTracer("router", "127.0.0.1:6831")
 
 	if err := webService.Run(); err != nil {
 		fmt.Println(err)
--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/lenny-mo/router/handler/cart"
 	"github.com/lenny-mo/router/handler/order"
 	"github.com/lenny-mo/router/handler/payment"
+	"github.com/lenny-mo/router/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +15,7 @@ func InitRouters() *gin.Engine {
 	// payment router group
 	// // 暴露三个接口：结算订单，查询支付状态，更新支付状况
 	paymentGroup := ginRouter.Group("/payment")
+	paymentGroup.Use(middleware.TracerWrapper)
 	{
 		paymentGroup.POST("/create", payment.CreatePaymentHandler)
 		paymentGroup.GET("/get", payment.GetPaymentHandler)
@@ -23,6 +25,7 @@ func InitRouters() *gin.Engine {
 	// order router group
 	// 暴露三个接口：创建订单，查询订单，更新订单
 	orderGroup := ginRouter.Group("/order")
+	orderGroup.Use(middleware.TracerWrapper) // 添加链路追踪
 	{
 		orderGroup.POST("/create", order.CreateOrderHandler)
 		orderGroup.GET("/get", order.GetOrderHandler)
@@ -33,6 +36,7 @@ func InitRouters() *gin.Engine {
 	// cart router group
 	// 暴露三个接口：加购，查询购物车
 	cartGroup := ginRouter.Group("/cart")
+	cartGroup.Use(middleware.TracerWrapper)
 	{
 		cartGroup.POST("/add", cart.CreateCartHandler)
 		cartGroup.GET("/findall", cart.GetCartHandler)
